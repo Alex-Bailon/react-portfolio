b7932d840f6e9e16f1894db93e18e4b2bdb695c5
Alex B <abailon949@gmail.com>
2020-10-30 20:32:43 -0700

    contact fixes

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -14,10 +14,10 @@ export default {
       Contact
     </v-card-title>
     <v-card-text>
-      Phone: <a href="tel:+7739493259">(773)949-3259</a> <br/>
+      Phone: <a href="tel:+17739493259">(773)949-3259</a> <br/>
       Email: <a href="mailto:abailon949@gmail.com">abailon949@gmail.com</a> <br/>
       Linked-In: <a href="https://www.linkedin.com/in/alex-bailon" target="_blank">Alex-Bailon</a><br/>
-      GitHub: <a href="http://https://github.com/Alex-Bailon" target="_blank">Alex-Bailon</a>
+      GitHub: <a href="https://github.com/Alex-Bailon" target="_blank">Alex-Bailon</a>
     </v-card-text>
   </v-card>
 </template>
